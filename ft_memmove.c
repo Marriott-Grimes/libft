982afa2a74c7b887422f53622163219ee3245c12
Matt Grimes <marriottgrimes@gmail.com>
2016-09-26 20:22:31 -0700

    first commit lol

---
+++ ft_memmove.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgrimes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/25 12:50:59 by mgrimes           #+#    #+#             */
+/*   Updated: 2016/09/25 12:51:14 by mgrimes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t			i;
+	unsigned char	*dest;
+	unsigned char	*source;
+	int				count;
+
+	i = 0;
+	dest = (unsigned char *)dst;
+	source = (unsigned char *)src;
+	count = len;
+	if (dest < source)
+	{
+		while (i++ < len)
+			dest[i] = source[i];
+	}
+	else
+	{
+		while (count-- >= 0)
+		{
+			dest[count] = source[count];
+		}
+	}
+	return (dst);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgrimes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/22 20:32:25 by mgrimes           #+#    #+#             */
+/*   Updated: 2016/09/22 20:32:27 by mgrimes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_min(int a, int b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+size_t		ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] && i < size)
+		i++;
+	while (src[j] && i + j < size)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i + j < size - 1)
+	{
+		j++;
+		dst[i + j] = 0;
+	}
+	return (ft_strlen(src) + ft_min(ft_strlen(dst), size));
+}
